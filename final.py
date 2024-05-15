566560255a3669eab7f97148bb5015231624b190
Francisco Pimentel Serra <franciscopimentelserra@gmail.com>
2024-05-16 00:59:52 +0100

    Add more functions

--- final.py
+++ final.py
@@ -27,11 +27,45 @@ def get_most_important_features(dataset: pd.DataFrame, nr_columns: int) -> list:
     selected_indices = np.append(selected_indices, last_column_index)
     
     return selected_indices
-# main function
+
+def reduce_dataset(dataset: pd.DataFrame, nr_rows: int) -> pd.DataFrame:
+    target_column = dataset.columns[-1]
+    target_column.value_counts(normalize=True)
+    
+    target_class_percentage = {}
+    for i in target_column.value_counts().index:
+        target_class_percentage[i] = target_column.value_counts(normalize=True)[i], (nr_rows * target_column.value_counts(normalize=True)[i]).round(0)
+    #! from here ...
+    target_class_count = dict.fromkeys(target_column.unique(), 0)
+
+    #remove 6. This was because in the beginning we were only going to choose 6 values from each class
+    # this needs to be dynamic and calculate in order to maintain the class distribution
+    lowest_percentage = sorted(target_class_percentage.items(), key=lambda x: x[1][0])[:6]
+
+    for lett in lowest_percentage:
+        classes_to_remove = lett[0]
+        target_class_count[classes_to_remove] += 1
+    #! to here is just example code
+    return dataset.iloc[:nr_rows]
+
+
+#* Main function
+
+#* Assumptions:
+#* - The last column is the target column
+#* - The datasets only have numerical features (except the target column which can be categorical)
+#* - The datasets might have a different number of rows and columns
 def analyse_datasets_accuracies(initial_dataset: pd.DataFrame, final_dataset: pd.DataFrame, percentage: int):
     min_rows, min_columns = get_final_dataset_dimensions(initial_dataset, final_dataset)
     initial_dataset_features = get_most_important_features(initial_dataset, min_columns)
     final_dataset_features = get_most_important_features(final_dataset, min_columns)
+
+    # Change the datasets to have the same number of columns
+    initial_dataset = initial_dataset.iloc[:, initial_dataset_features]
+    final_dataset = final_dataset.iloc[:, final_dataset_features]
+
+    initial_dataset = reduce_dataset(initial_dataset, min_rows)
+    final_dataset = reduce_dataset(final_dataset, min_rows)
     print("Analyse datasets accuracies")
 
 analyse_datasets_accuracies(pd.read_csv('csv/Iris.csv'), pd.read_csv('csv/raisin.csv'), 10)
