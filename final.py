0971a2e4beb5c4c6c5f04de17a14867ff3eec323
Francisco Pimentel Serra <franciscopimentelserra@gmail.com>
2024-05-16 15:47:21 +0100

    Dataset is now being ordered with the same order as the dict

--- final.py
+++ final.py
@@ -2,6 +2,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import pandas as pd
+from example_T12 import start
+from sklearn.preprocessing import LabelEncoder
+
+#start.analyse_datasets_accuracies(pd.read_csv('csv/Iris.csv'), pd.read_csv('csv/raisin.csv'), 10)
 
 def get_final_dataset_dimensions(initial_dataset: pd.DataFrame, final_dataset: pd.DataFrame) -> tuple:
     di_rows, di_columns = initial_dataset.shape
@@ -28,19 +32,37 @@ def get_most_important_features(dataset: pd.DataFrame, nr_columns: int) -> list:
     
     return selected_indices
 
+def organize_dataset(dataset: pd.DataFrame, target_class_percentage: dict) -> pd.DataFrame:
+    ordered_data_parts = []
+    for key in target_class_percentage.keys():
+        # Filter the dataset for the current class
+        class_data = dataset[dataset[dataset.columns[-1]] == key]
+        # Append the filtered data to the list
+        ordered_data_parts.append(class_data)
+
+    # Concatenate the parts together to get the ordered dataset
+    new_dataset = pd.concat(ordered_data_parts, ignore_index=True)
+    return new_dataset
+
 def reduce_dataset(dataset: pd.DataFrame, nr_rows: int) -> pd.DataFrame:
-    target_column = dataset.columns[-1]
+    target_column = dataset[dataset.columns[-1]]
     target_column.value_counts(normalize=True)
     
     target_class_percentage = {}
     for i in target_column.value_counts().index:
-        target_class_percentage[i] = target_column.value_counts(normalize=True)[i], (nr_rows * target_column.value_counts(normalize=True)[i]).round(0)
+        target_class_percentage[i] = target_column.value_counts(normalize=True)[i], round(nr_rows * target_column.value_counts(normalize=True)[i])
+    dataset = organize_dataset(dataset, target_class_percentage)
+    print(target_class_percentage)
     #! from here ...
     target_class_count = dict.fromkeys(target_column.unique(), 0)
 
     #remove 6. This was because in the beginning we were only going to choose 6 values from each class
     # this needs to be dynamic and calculate in order to maintain the class distribution
     lowest_percentage = sorted(target_class_percentage.items(), key=lambda x: x[1][0])[:6]
+    for key, x, i in target_class_percentage:
+        for t in dataset:
+            if dataset['target'] == key:
+                new_dataset = new_dataset.append(dataset['target'])
 
     for lett in lowest_percentage:
         classes_to_remove = lett[0]
@@ -48,6 +70,10 @@ def reduce_dataset(dataset: pd.DataFrame, nr_rows: int) -> pd.DataFrame:
     #! to here is just example code
     return dataset.iloc[:nr_rows]
 
+def encode_labels(dataset: pd.DataFrame) -> pd.DataFrame:
+    label_encoder = LabelEncoder()
+    dataset[dataset.columns[-1]] = label_encoder.fit_transform(dataset[dataset.columns[-1]])
+    return dataset
 
 #* Main function
 
@@ -57,9 +83,14 @@ def reduce_dataset(dataset: pd.DataFrame, nr_rows: int) -> pd.DataFrame:
 #* - The datasets might have a different number of rows and columns
 def analyse_datasets_accuracies(initial_dataset: pd.DataFrame, final_dataset: pd.DataFrame, percentage: int):
     min_rows, min_columns = get_final_dataset_dimensions(initial_dataset, final_dataset)
+    initial_dataset = encode_labels(initial_dataset)
+    final_dataset = encode_labels(final_dataset)
+
     initial_dataset_features = get_most_important_features(initial_dataset, min_columns)
     final_dataset_features = get_most_important_features(final_dataset, min_columns)
 
+    print(initial_dataset_features)
+    print(final_dataset_features)
     # Change the datasets to have the same number of columns
     initial_dataset = initial_dataset.iloc[:, initial_dataset_features]
     final_dataset = final_dataset.iloc[:, final_dataset_features]
@@ -68,4 +99,4 @@ def analyse_datasets_accuracies(initial_dataset: pd.DataFrame, final_dataset: pd
     final_dataset = reduce_dataset(final_dataset, min_rows)
     print("Analyse datasets accuracies")
 
-analyse_datasets_accuracies(pd.read_csv('csv/Iris.csv'), pd.read_csv('csv/raisin.csv'), 10)
+analyse_datasets_accuracies(pd.read_csv('final_csv/reduced_iris_copy.csv'), pd.read_csv('final_csv/reduced_raisin_copy.csv'), 10)
